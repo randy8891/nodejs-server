53f1d7b85487b41779e82480f862055375cb16f8
Randika <randika.nonis@gmail.com>
2024-01-12 06:13:06 +0530

    server script

---
+++ app.js
@@ -0,0 +1,17 @@
+//import the http package
+const http = require('http');
+
+//create the http server
+const server = http.createServer((req, res) => {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Hello, Node.js server!');
+});
+
+
+//define port 3000
+const PORT = 3000;
+
+//get the server to listen
+server.listen(PORT, ()=> {
+	console.log('Server running at http://localhost:${PORT}/');
+});
